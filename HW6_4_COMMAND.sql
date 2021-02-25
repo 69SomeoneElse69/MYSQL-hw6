83c7284291bf61543de25b1fa8291bd23d3980a1
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-02-25 10:31:46 +0300

    Update HW6_4_COMMAND.sql

--- HW6_4_COMMAND.sql
+++ HW6_4_COMMAND.sql
@@ -1,7 +1,6 @@
-
 select *,
 (select TIMESTAMPDIFF(YEAR, birthday, NOW()) from profiles p where user_id = id) as age,
 (select birthday from profiles p2 where user_id=id) as birthday 
-from likes
+from likes where target_type_id = '2' 
 ORDER BY age
 LIMIT 10;
